f08d244dc3a86cae3b74ad497a06f1974432fbf7
M1CH4EL-ux2 <joaomichael912@gmail.com>
2021-12-29 12:53:18 -0300

    Projeto finalizado com sucesso, tudo prontinho

--- src/pages/Film.js
+++ src/pages/Film.js
@@ -2,23 +2,54 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react"
 import '../sass/Film.scss'
 
-export function Film() {
-    const { id } = useParams();
+function getWindowDimensions() {
+    const {innerWidth: width, innerHeight: height} = window;
 
+    return {
+        width,
+        height
+    }
+}
+
+function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+  
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    return windowDimensions;
+}
+
+export function Film() {
     const [data, setData] = useState([]);
+    const [size, setSize] = useState(false);
+    const { id } = useParams();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(async () => {
         const response = await fetch(`https://ghibliapi.herokuapp.com/films/${id}`);
         const dataResponse = await response.json();
         setData(dataResponse)
-    }, [])
+    }, [id])
+
+    const sizing = useWindowDimensions()
+    useEffect(() => {
+        if(sizing.width >= 700) {
+            setSize(true)
+        }
+    }, [sizing.width])
 
     console.log(data)
 
     return (
         <div className="container-film">
-            <img src={data.image} />
+            <img src={size ? data.movie_banner : data.image} alt={data.title} />
 
             <div className="content">
                 <h1>{data.title}</h1>
